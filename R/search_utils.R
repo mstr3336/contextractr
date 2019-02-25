9c5c03755b4fb20e80edc12d217accf347099033
Matthew Mark Strasiotto <mstr3336@uni.sydney.edu.au>
2019-02-25 11:48:35 +1100

    renamed remove_false_positives

--- R/search_utils.R
+++ R/search_utils.R
@@ -22,7 +22,9 @@ find_keywords <- function(col, json.input){
   return(indexer)
 }
 
-remove_false_positives <- function(col, sep, indexer){
+
+
+index_kws_in_strings <- function(col, sep, indexer){
   split_col <- col %>% rlang::as_list()
   if (!is.na(sep)) split_col %<>% stringr::str_split(sep)
 
@@ -30,13 +32,19 @@ remove_false_positives <- function(col, sep, indexer){
     dplyr::mutate(kw_pos = purrr::pmap(
       .,
       function(keywords, approx.match, match_locs){
+        # iterate through these columns
         out <- purrr::pmap(
           list(keywords, approx.match, match_locs),
-          function(keywords, approx.match, match_locs, ...){
+          function(keyword, approx.match, match_locs, ...){
+            # within each row, there are vectors of
+            # keywords[n], approx.match[n],
+            # & match_locs[n], which will nest a variable vector of integers
+            # split_col should be tokenized s.t it is first split into spans,
+            # then into words (so split col is list->list->chr
             cvecs <- split_col[match_locs]
 
             positions <- cvecs %>%
-              purrr::map(~ agrep(keywords, ., max.distance = approx.match)) %>%
+              purrr::map(~ agrep(keyword, ., max.distance = approx.match)) %>%
               purrr::set_names(nm = as.character(match_locs))
             return(positions)
           }
