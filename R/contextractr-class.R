e510cfa729cddc8e2f78fee0ddcdfaf75f4f9b82
Matthew Mark Strasiotto <mstr3336@uni.sydney.edu.au>
2019-03-06 16:59:44 +1100

    now locate_keywords returns all matched groups

--- R/contextractr-class.R
+++ R/contextractr-class.R
@@ -142,7 +142,7 @@ Contextractr$set(
     selection <- rlang::enquo(selection)
     col <- selection %>% rlang::as_name()
 
-    group_col <- glue("{col}_group") %>% as.character()
+    group_col <- glue("{col}_groups") %>% as.character()
     kw_col    <- glue("{col}_keywords") %>% as.character()
 
     idx <- .df %>%
@@ -151,7 +151,7 @@ Contextractr$set(
 
     out <- .df %>% private$add_keyword_cols(col, idx)
     out %<>%
-      dplyr::mutate(!!group_col := purrr::map(indexer, list("title",1)))
+      dplyr::mutate(!!group_col := purrr::map(indexer, "title"))
     out %<>%
       dplyr::mutate_at(group_col,
                        ~purrr::map_chr(.,null_as_na))
