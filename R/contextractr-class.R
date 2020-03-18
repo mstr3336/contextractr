583a51ff80830399aa706621496473485b57ef4f
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-03-18 13:20:02 +1100

    Move add_keyword_cols to separate function from class

--- R/contextractr-class.R
+++ R/contextractr-class.R
@@ -172,7 +172,7 @@ Contextractr$set(
       dplyr::pull(col) %>%
       private$find_keywords(private$mapping)
 
-    out <- .df %>% private$add_keyword_cols(col, idx)
+    out <- .df %>% add_keyword_cols(col, idx)
     out %<>%
       dplyr::mutate(!!group_col := purrr::map(indexer, "title")) %>%
       dplyr::mutate_at(group_col, ~ purrr::map(., unique))
@@ -284,37 +284,6 @@ Contextractr$set(
         }))
 
     return(indexer)
-  }
-)
-
-# add_keyword_cols ====
-
-#' @importFrom glue glue
-#' @importFrom rlang quo quos enquo enquos
-
-Contextractr$set(
-  "private", "add_keyword_cols",
-  function(.df, col,indexer){
-
-    # By unnesting keywords, approxmatch, match_locs
-    # and unnesting match_locs again, a unique index for each row is made
-    unnest_cols <- rlang::quos(keywords, approx.match, match_locs)
-    preserve <-  indexer %>% names() %>%
-      setdiff(purrr::map_chr(unnest_cols,rlang::as_name))
-
-    indexer %<>%
-      tidyr::unnest(!!!unnest_cols, .preserve = dplyr::one_of(preserve)) %>%
-      dplyr::mutate_at(dplyr::vars(!!!unnest_cols[1:2]), unlist) %>%
-      tidyr::unnest(!!!unnest_cols[3], .preserve = dplyr::one_of(preserve))
-
-    # We need idx so we can join it on match locs
-    # A nested tibble col will be added for each corresponding match
-    out <- .df %>%
-      dplyr::mutate(idx = dplyr::row_number()) %>%
-      dplyr::nest_join(indexer, by = c("idx" = "match_locs"), name = "indexer")
-
-    return(out)
-
   }
 )
 
