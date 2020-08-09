30aa64fdf65d80ad023ddc05307d4fc35e66556d
Matthew Strasiotto <mstr3336@uni.sydney.edu.au>
2020-08-09 21:29:16 +1000

    Change "unnest" calls to use new tidyr::unnest api

--- R/extraction_helpers.R
+++ R/extraction_helpers.R
@@ -7,14 +7,14 @@ add_keyword_cols <- function(.df, col,indexer){
 
   # By unnesting keywords, approxmatch, match_locs
   # and unnesting match_locs again, a unique index for each row is made
-  unnest_cols <- rlang::quos(keywords, approx.match, match_locs)
+  unnest_cols <- c("keywords", "approx.match", "match_locs")
   preserve <-  indexer %>% names() %>%
     setdiff(purrr::map_chr(unnest_cols,rlang::as_name))
 
   indexer %<>%
-    tidyr::unnest(!!!unnest_cols, .preserve = dplyr::one_of(preserve)) %>%
-    dplyr::mutate_at(dplyr::vars(!!!unnest_cols[1:2]), unlist) %>%
-    tidyr::unnest(!!!unnest_cols[3], .preserve = dplyr::one_of(preserve))
+    tidyr::unnest(unnest_cols, .preserve = dplyr::one_of(preserve)) %>%
+    dplyr::mutate_at(unnest_cols[1:2], unlist) %>%
+    tidyr::unnest(unnest_cols[3], .preserve = dplyr::one_of(preserve))
 
   # We need idx so we can join it on match locs
   # A nested tibble col will be added for each corresponding match
